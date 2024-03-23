dc53dfeff759445a8f17ca52013ada9aa5a56f78
Adeonir Kohl <adeonir@gmail.com>
2024-03-23 10:34:24 -0300

    Add button to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,36 @@ void main() {
 class RollTheDice extends StatelessWidget {
   const RollTheDice({super.key});
 
+  void onRollDice() {
+    print('Button pressed');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Roll the Dice',
       home: Scaffold(
         body: GradientContainer(
-          child: Image.asset(
-            'assets/images/dice-1.png',
-            width: 200,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/dice-1.png',
+                width: 240,
+              ),
+              const SizedBox(height: 24),
+              TextButton(
+                onPressed: onRollDice,
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.indigoAccent,
+                  padding: const EdgeInsets.only(left: 24, right: 24),
+                  textStyle: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w600),
+                ),
+                child: const Text('Roll the Dice'),
+              ),
+            ],
           ),
         ),
       ),

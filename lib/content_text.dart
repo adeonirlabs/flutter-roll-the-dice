7ee8f44904031077943804157261208ba00fbfa5
Adeonir Kohl <adeonir@gmail.com>
2024-03-23 08:55:18 -0300

    Split styled text to its own file

---
+++ lib/content_text.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Hello World!',
+      style: TextStyle(fontSize: 48, color: Colors.white),
+    );
+  }
+}
--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roll_the_dice/content_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -14,10 +15,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(fontSize: 48, color: Colors.white),
-        ),
+        child: StyledText(),
       ),
     );
   }
